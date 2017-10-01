3de3b17b2fd8a8deccba7387e0c7e7ccd9a26fd2
alchemyofdeath <doru_metalguitar@yahoo.com>
2017-10-01 14:35:18 +0300

    Add files via upload

---
+++ SnakeGame/include/arduino/DoruFunctions.cpp
@@ -0,0 +1,13 @@
+/*
+ * CPPFile1.cpp
+ *
+ * Created: 2/13/2016 6:14:30 PM
+ *  Author: Irimescu
+ */ 
+#include "ProjectHeader.h"
+
+ISR(BADISR_vect)
+{//unhandeled interrupt
+	lcd.setCursor(0,0);
+	lcd.print("Unexpected error");
+}
